42210dbc9a14ac4380ade11f2741a77c78fb3b1a
jjn77 <43833006+jjn77@users.noreply.github.com>
2020-11-12 10:38:22 -0600

    Add files via upload

--- BlackScholesAlgo.py
+++ BlackScholesAlgo.py
@@ -1,9 +1,10 @@
 import math
 import numpy as np
-import scipy.stats
+from scipy.stats import norm 
 
 
 def blackScholes(spotPrice, strikePrice, time, riskFreeRate, volatility):
-	day1 = (np.log(spotPrice/strikePrice) + (riskFreeRate + volatility**volatility/2)**time)/(volatility**np.sqrt(time))
-	day2 = day1 - volatility**np.sqrt(time)
-	return spotPrice * scipy.stats.norm.cdf(day1) - strikePrice*np.exp(-1**riskFreeRate**time)**scipy.stats.norm.cdf(day2)
+	day1 = (np.log(spotPrice/strikePrice) + (riskFreeRate + (volatility**2)/2)*time)/(volatility*np.sqrt(time))
+	day2 = day1 - volatility*np.sqrt(time)
+	estimate = spotPrice * norm.cdf(day1) - strikePrice*np.exp(-1*riskFreeRate*time)*norm.cdf(day2)
+	return estimate
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import credentials
 import datetime
 import math
 import numpy as np
+import time
 import pandas as pd
 import scipy.stats
 import BlackScholesAlgo
@@ -11,43 +12,34 @@ client = Robinhood()
 
 client.login(credentials.name, credentials.passw)
 
-    #Note: Sometimes more than one instrument may be returned for a given stock symbol
-stock_instrument = client.instruments("AMC")[0]
-print(stock_instrument)
-print('\n')
+while(True): 
+	spotPrice = float(client.quote_data("AMC")['ask_price'])
+	print("Spot Price -")
+	print(spotPrice)
 
-#Get a stock's quote
-print(client.print_quote("AMC"))
-print('\n')
+	print("Strike Price -")
+	strikePrice = 3
+	print(strikePrice)
 
-#View all data for a given stock ie. Ask price and size, bid price and size, previous close, adjusted previous close, etc.
-quote_info = client.quote_data("AMC")
-print(quote_info)
+	print("Risk Free Rate -")
+	riskFreeRate = abs(float(client.quote_data("AMC")['bid_price'])/float(client.quote_data("AMC")['previous_close']) - 1)
+	print(riskFreeRate)
 
-spotPrice = float(client.quote_data("AMC")['ask_price'])
-print("Spot Price -")
-print(spotPrice)
+	print("Time (in years) -")
+	stockTime = abs((7 - datetime.datetime.now().day)/250)
+	print(stockTime)
 
-print("Strike Price -")
-strikePrice = 3
-print(strikePrice)
+	print("Volatility -")
+	volatility = np.sqrt(stockTime)*riskFreeRate
+	print(volatility)
 
-print("Risk Free Rate -")
-riskFreeRate = float(client.quote_data("AMC")['bid_price'])/float(client.quote_data("AMC")['previous_close']) - 1
-print(riskFreeRate)
+	print("Call Option Estimate -")
+	answer = BlackScholesAlgo.blackScholes(spotPrice, strikePrice, stockTime, riskFreeRate, volatility)
+	print(answer)
 
-print("Time (in years) -")
-time = abs((7 - datetime.datetime.now().day)/250)
-print(time)
+	print("Call Option Actual Value -")
+	shareAmnt = (client.equity()-48.56)/100
+	print(shareAmnt)
+	
+	time.sleep(7200)
 
-print("Volatility -")
-volatility = np.sqrt(time)*riskFreeRate
-print(volatility)
-
-print("Call Option Estimate -")
-answer = BlackScholesAlgo.blackScholes(spotPrice, strikePrice, time, riskFreeRate, volatility)
-print(answer)
-
-print("Call Option Actual Value -")
-shareAmnt = (client.equity()-48.56)/100
-print(shareAmnt)
